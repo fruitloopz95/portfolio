9875c2a08c78186ca621fc8c83c325dcd5df36c0
fruitloopz95 <furqaan.ismail10@gmail.com>
2020-12-11 15:34:35 +0200

    updated gtag location

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react'
 import Router from 'next/router'
 
-import * as gtag from 'common/src/lib/gtag'
+import * as gtag from '../lib/gtag'
 
 // Notice how we track pageview when route is changed
 Router.events.on('routeChangeComplete', (url) => gtag.pageview(url))
